d37a96c36675930fbdb294266f82257c0e52bc60
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2024-10-12 12:36:35 -0300

    implementação das camadas de employee

---
+++ src/main/java/com/backend/services/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.backend.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backend.entities.Employee;
+import com.backend.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository repository;
+	
+	public List<Employee> findAll() {
+		return repository.findAll();
+	}
+}
