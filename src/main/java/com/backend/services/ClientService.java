a289138e152712d1c3a02e7d117156bead0e7298
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2024-10-13 12:24:39 -0300

    getById de Client

--- src/main/java/com/backend/services/ClientService.java
+++ src/main/java/com/backend/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.backend.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,11 @@ public class ClientService {
 	
 	public List<Client> findAll() {
 		return repository.findAll();
+	}
+	
+	public Client findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		return obj.get();
 	}
 	
 }
