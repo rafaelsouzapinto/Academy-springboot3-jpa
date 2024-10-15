3e055f4281d5d8f50a63b0fcbf05ed0f69e172c1
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2024-10-15 18:51:05 -0300

    implementação das operações de payment

--- src/main/java/com/backend/controllers/PaymentController.java
+++ src/main/java/com/backend/controllers/PaymentController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +27,29 @@ public class PaymentController {
 	public ResponseEntity<List<Payment>> findAll() {
 		List<Payment> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Payment> findById(@PathVariable Long id) {
+		Payment payment = service.findById(id);
+		return ResponseEntity.ok().body(payment);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Payment> insert(@RequestBody Payment obj) {
+		Payment payment = service.insert(obj);
+		return ResponseEntity.ok().body(payment);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Payment> update (@RequestBody Payment obj, @PathVariable Long id) {
+		Payment payment = service.update(obj, id);
+		return ResponseEntity.ok().body(payment);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete (@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/backend/services/PaymentService.java
+++ src/main/java/com/backend/services/PaymentService.java
@@ -1,6 +1,7 @@
 package com.backend.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,28 @@ public class PaymentService {
 	
 	public List<Payment> findAll() {
 		return repository.findAll();
+	}
+	
+	public Payment findById(Long id) {
+		Optional<Payment> payment = repository.findById(id);
+		return payment.get();
+	}
+	
+	public Payment insert(Payment obj) {
+		return repository.save(obj);
+	}
+	
+	public Payment update (Payment obj, Long id) {
+		Payment payment = findById(id);
+		payment.setEnrollment(obj.getEnrollment());
+		payment.setPaymentDate(obj.getPaymentDate());
+		payment.setPaymentMethod(obj.getPaymentMethod());
+		payment.setPaymentStatus(obj.getPaymentStatus());
+		payment.setPaymentValue(obj.getPaymentValue());
+		return repository.save(payment);
+	}
+	
+	public void delete (Long id) {
+		repository.deleteById(id);
 	}
 }
